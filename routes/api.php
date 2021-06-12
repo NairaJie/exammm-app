51cf792950c4f9a3f098c45da2a4ece1c4e65192
ailaa jie <nairaxaviera@gmail.com>
2021-06-12 21:53:47 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+
+/*
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//GET untuk mendapatkan user by id
+Route::get('user/{id}', [UserController::class, 'getUserById']);
+
+//GET untuk mendapatkan semua user
+Route::get('users', [UserController::class, 'getAllUsers']);
+
+//POST untuk menambahkan data user
+Route::post('users', [UserController::class, 'createUser' ]);
+
+//POST untuk mengupdate data user
+Route::put('user/{id}', [UserController::class, 'updateUser']);
+
+//DELETE untuk menghapus data user
+Route::delete('user/{id}', [UserCOntroller::class, 'deleteUser']);
